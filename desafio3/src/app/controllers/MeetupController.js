d2475ec731f97a0c83737af502d73474d9452138
ricardo <ricardo85x@gmail.com>
2019-08-16 09:51:56 -0300

    desafio9 - wip

--- desafio3/src/app/controllers/MeetupController.js
+++ desafio3/src/app/controllers/MeetupController.js
@@ -102,6 +102,8 @@ class MeetupController {
       .catch(err => Promise.resolve(err));
 
     if (validacao.name === 'ValidationError') {
+      console.log('error 1', validacao.errors);
+      console.log(req.body);
       return res.status(400).json({
         error: 'Erro na validação',
         msg: validacao.errors,
@@ -113,6 +115,8 @@ class MeetupController {
     const hourStart = startOfHour(parseISO(date));
 
     if (isBefore(hourStart, new Date())) {
+      console.log('error 2');
+
       return res.status(400).json({
         error:
           'It is not possible to create meetup before current time or same hour as now',
@@ -123,6 +127,8 @@ class MeetupController {
     const bannerExists = await File.findByPk(banner);
 
     if (!bannerExists) {
+      console.log('error 3');
+
       return res.status(400).json({
         error: 'banner id not found',
       });
